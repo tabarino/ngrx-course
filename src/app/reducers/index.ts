03ea30ec2305e3493a049aad1e273dbe27f72774
tabarino <tabarino@outlook.com>
2020-05-08 23:50:44 +0100

    NgRx Metareducers

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -15,4 +15,13 @@ export const reducers: ActionReducerMap<AppState> = {
     router: routerReducer
 };
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
+export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
+    return (state, action) => {
+        console.log('state before: ', state);
+        console.log('current action: ', action);
+
+        return reducer(state, action);
+    };
+}
+
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [logger] : [];
