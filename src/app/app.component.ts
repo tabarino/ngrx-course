4f4892cfbe3b58aacee2231dca637274a9c753d2
tabarino <tabarino@outlook.com>
2020-05-08 18:56:55 +0100

    NgRx Selectors - An In-Depth Explanation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 import { AppState } from './reducers';
+import { isLoggedIn, isLoggedOut } from './auth/auth.selectors';
 
 @Component({
     selector: 'app-root',
@@ -39,11 +39,11 @@ export class AppComponent implements OnInit {
         });
 
         this.isLoggedIn$ = this.store.pipe(
-            map(state => !!state['auth'].user)
+            select(isLoggedIn)
         );
 
         this.isLoggedOut$ = this.store.pipe(
-            map(state => !state['auth'].user)
+            select(isLoggedOut)
         );
     }
 
