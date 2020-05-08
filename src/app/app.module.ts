1a301a1e9c5d6646f0b5bb8ff2520984e1b5ffe8
tabarino <tabarino@outlook.com>
2020-05-08 23:04:35 +0100

    Setting up NgRx Router Store and the Time-Travelling Debugger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,9 +15,8 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { RouterState, StoreRouterConnectingModule } from '@ngrx/router-store';
 import { EffectsModule } from '@ngrx/effects';
-import { EntityDataModule } from '@ngrx/data';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { reducers, metaReducers } from './reducers';
+import { metaReducers, reducers } from './reducers';
 import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
@@ -50,7 +49,11 @@ const routes: Routes = [
         AuthModule.forRoot(),
         StoreModule.forRoot(reducers, { metaReducers }),
         StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
-        EffectsModule.forRoot([])
+        EffectsModule.forRoot([]),
+        StoreRouterConnectingModule.forRoot({
+            stateKey: 'router',
+            routerState: RouterState.Minimal
+        })
     ],
     bootstrap: [AppComponent]
 })
