244e843fdc7f011629ae14c76f2abdcfdb8b334c
tabarino <tabarino@outlook.com>
2020-05-08 12:32:52 +0100

    Grouping Actions Together with Action Types

--- src/app/auth/auth.actions.ts
+++ src/app/auth/auth.actions.ts
@@ -4,3 +4,7 @@ import { User } from './model/user.model';
 export const login = createAction(
     '[Login Page] User Login', props<{user: User}>()
 );
+
+export const logout = createAction(
+    '[Top Menu] Logout'
+);
