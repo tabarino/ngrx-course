044c9a562e7263d75913e6509e501eda6de28060
tabarino <tabarino@outlook.com>
2020-05-08 17:12:22 +0100

    NgRx Actions and Reducers In Action

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -36,10 +36,8 @@ export class LoginComponent implements OnInit {
         const val = this.form.value;
         this.auth.login(val.email, val.password).pipe(
             tap(user => {
-                console.log(user);
-
-                this.store.dispatch(login({ user }));
-
+                const newLoginAction = login({ user });
+                this.store.dispatch(newLoginAction);
                 this.router.navigateByUrl('/courses');
             })
         ).subscribe(
