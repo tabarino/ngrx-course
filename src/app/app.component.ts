6f498b1d8ac9c70398834b5a394ccf152204518c
tabarino <tabarino@outlook.com>
2020-05-08 22:32:08 +0100

    Implementing the Logout Effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 import { AppState } from './reducers';
 import { isLoggedIn, isLoggedOut } from './auth/auth.selectors';
-import { logout } from './auth/auth.actions';
+import { login, logout } from './auth/auth.actions';
 
 @Component({
     selector: 'app-root',
@@ -20,6 +20,12 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit() {
+        const userProfile = localStorage.getItem('user');
+
+        if (userProfile) {
+            this.store.dispatch(login({ user: JSON.parse(userProfile) }));
+        }
+
         this.router.events.subscribe(event => {
             switch (true) {
                 case event instanceof NavigationStart: {
