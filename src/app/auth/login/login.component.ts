d5a931f184768ce2b4a321f088ed4b2807b95dca
tabarino <tabarino@outlook.com>
2020-05-08 11:39:40 +0100

    The Store Service API - Implementing the Login Screen

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,41 +1,48 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-
-import {Store} from "@ngrx/store";
-
-import {AuthService} from "../auth.service";
-import {tap} from "rxjs/operators";
-import {noop} from "rxjs";
-import {Router} from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AuthService } from '../auth.service';
+import { tap } from 'rxjs/operators';
+import { noop } from 'rxjs';
+import { Router } from '@angular/router';
+import { AppState } from '../../reducers';
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+    form: FormGroup;
 
-  form: FormGroup;
+    constructor(
+        private fb: FormBuilder,
+        private auth: AuthService,
+        private router: Router,
+        private store: Store<AppState>
+    ) {
+        this.form = fb.group({
+            email: ['test@angular-university.io', [Validators.required]],
+            password: ['test', [Validators.required]]
+        });
+    }
 
-  constructor(
-      private fb:FormBuilder,
-      private auth: AuthService,
-      private router:Router) {
+    ngOnInit() {
 
-      this.form = fb.group({
-          email: ['test@angular-university.io', [Validators.required]],
-          password: ['test', [Validators.required]]
-      });
+    }
 
-  }
+    login() {
+        const val = this.form.value;
+        this.auth.login(val.email, val.password).pipe(
+            tap(user => {
+                console.log(user);
 
-  ngOnInit() {
+                // this.store.dispatch();
 
-  }
-
-  login() {
-
-  }
-
+                this.router.navigateByUrl('/courses');
+            })
+        ).subscribe(
+            noop, () => alert('Login Failed')
+        );
+    }
 }
-
