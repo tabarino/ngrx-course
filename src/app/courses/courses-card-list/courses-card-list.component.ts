2eb93ddbc3da9b1437231e703476b824d1b363fb
tabarino <tabarino@outlook.com>
2020-05-09 16:25:09 +0100

    Optimistically Editing Entity Data - The Edit Course Dialog

--- src/app/courses/courses-card-list/courses-card-list.component.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.ts
@@ -1,8 +1,8 @@
-import {Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
-import {Course} from "../model/course";
-import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
-import {EditCourseDialogComponent} from "../edit-course-dialog/edit-course-dialog.component";
-import {defaultDialogConfig} from '../shared/default-dialog-config';
+import { Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import { Course } from '../model/course';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { EditCourseDialogComponent } from '../edit-course-dialog/edit-course-dialog.component';
+import { defaultDialogConfig } from '../shared/default-dialog-config';
 
 @Component({
     selector: 'courses-card-list',
@@ -10,49 +10,32 @@ import {defaultDialogConfig} from '../shared/default-dialog-config';
     styleUrls: ['./courses-card-list.component.css']
 })
 export class CoursesCardListComponent implements OnInit {
-
     @Input()
     courses: Course[];
 
     @Output()
     courseChanged = new EventEmitter();
 
-    constructor(
-      private dialog: MatDialog ) {
+    constructor(private dialog: MatDialog) {
     }
 
     ngOnInit() {
-
     }
 
-    editCourse(course:Course) {
-
+    editCourse(course: Course) {
         const dialogConfig = defaultDialogConfig();
 
         dialogConfig.data = {
-          dialogTitle:"Edit Course",
-          course,
-          mode: 'update'
+            dialogTitle: 'Edit Course',
+            course,
+            mode: 'update'
         };
 
-        this.dialog.open(EditCourseDialogComponent, dialogConfig)
-          .afterClosed()
-          .subscribe(() => this.courseChanged.emit());
-
+        this.dialog.open(EditCourseDialogComponent, dialogConfig).afterClosed().subscribe(
+            () => this.courseChanged.emit()
+        );
     }
 
-  onDeleteCourse(course:Course) {
-
-
-  }
-
+    onDeleteCourse(course: Course) {
+    }
 }
-
-
-
-
-
-
-
-
-
